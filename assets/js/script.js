1b05660e0c3fd465f2eba2c4161581d4ab42e6e3
Your Name <golamfaruk204@gmail.com>
2022-10-05 12:03:50 +0600

    Davis HTML Template is a personal portfolio site for personal usage

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+ 
+ /*--------------------------------------------------------------
+    10. Slick Slider
+  --------------------------------------------------------------*/ 
+
+  $( document ).ready(function() {
+    $('.st-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+
+
+
